e8030902ceeb6a60f5d8f6669fc6271b760e9221
Chris Mulligan <clm2186@columbia.edu>
2012-10-12 14:04:03 -0400

    Fixed up spelling... hopefully!

--- basicModel.R
+++ basicModel.R
@@ -8,10 +8,15 @@ training$avg_length <- training$num_chars / training$num_words
 training$avg_syls <- training$num_syl / training$num_words
 training$spell_pct <- training$num_correctly_spelled / training$num_words
 
+training$domain1_score_log <- log(training$domain1_score)
+hist(training$domain1_score_log)
 plot(training$spell_pct, training$domain1_score)
 
 cor(training, use="complete.obs")
 
-model <- lm(domain1_score ~ num_chars + avg_length + avg_syls + spell_mistakes + spell_pct, data=training)
+model <- lm(domain1_score ~ num_chars + log(num_chars) + avg_length + avg_syls  + spell_pct + starts_with_dear + spell_mistakes, data=training)
 model
 summary(model)
+
+step <- stepAIC(model, direction="both")
+step$anova
--- basic_tags.py
+++ basic_tags.py
@@ -8,6 +8,7 @@ from nltk.corpus import wordnet
 import enchant
 import argparse
 import random
+import re
 
 syl = syllables.cmusyllables()
 syl.Load()
@@ -15,9 +16,19 @@ syl.Load()
 enchantDict = enchant.Dict("en_US")
 
 PUNCTUATION = set(('.', ',', '"', "'", '`', ':', ';', '!', '~', '-', '=', '+', '?',
-    '(', ')', '[', ']', '{', '}', '<', '>', '*', '^', '%', '_', '|', "@",
+    '(', ')', '[', ']', '{', '}', '<', '>', '*', '^', '%', '_', '|', "@", "`",
     '\xe2', '\x80', '\x98', '\xe2', '\x80', '\x99', '\xe2', '\x80', '\x9c', '\xe2', '\x80', '\x9d'
     ))
+
+CONTRACTIONS = set(("'s", "wo", "n't", "'re", "'m", "'ve", "'ll", "isn"))
+websites = ("myspace", "facebook", "youtube", "e-mail", "google", "hand-eye", "eye-hand", "webcam", "microsoft", "caps1", "yahoo")
+SPECIAL_WORDS = set(("e-mail", "hand-eye", "eye-hand", "webcam", "skype", "america"))
+SPECIAL_WORDS.update(websites)
+SPECIAL_WORDS.update(x + ".com" for x in websites)
+SPECIAL_WORDS.update("www." + x + ".com" for x in websites)
+
+NER_re = re.compile(r"""(?:organization|caps|date|percent|person|money|location|num|month|time)\d+$""")
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--max', '-n', type=int, help="Maximum number of lines before bailing")
@@ -28,11 +39,13 @@ maxRows = args.max
 sample = args.sample
 inputFilename = args.inputFilename
 
+
 input = csv.reader(open(inputFilename), delimiter="\t")
 header = input.next()
 
 keys = ["essay_id", "essay_set", "essay", "rater1_domain1", "rater2_domain1", "domain1_score",
-    "num_chars", "num_sents", "num_words", "num_syl", "num_correctly_spelled", "fk_grade_level"]
+    "num_chars", "num_sents", "num_words", "num_syl", "num_correctly_spelled", "fk_grade_level",
+    "starts_with_dear"]
 outputFilename = os.path.splitext(os.path.basename(inputFilename))[0] + "_tagged.csv"
 output = csv.DictWriter(open(outputFilename, "w"), keys)
 output.writerow(dict(zip(keys, keys)))
@@ -45,9 +58,10 @@ for row in input:
     result = dict(zip(
         ["essay_id", "essay_set", "essay", "rater1_domain1", "rater2_domain1", "domain1_score"],
         (row[0], row[1], row[2], row[3], row[4], row[6])))
-    text = row[2].strip().decode('cp1252')
     sys.stdout.write("\r %s#%s" % (row[1], row[0]))
     sys.stdout.flush()
+
+    text = row[2].strip().decode('cp1252').lower()
 
     result["num_chars"] = len(text)
 
@@ -59,18 +73,19 @@ for row in input:
     words = []
     for sent in words_in_sentances:
         for word in sent:
-            if word not in PUNCTUATION:
-                words.append(word)
-            elif not all(char in PUNCTUATION for char in word):
+            if word not in PUNCTUATION and not all(char in PUNCTUATION for char in word):
                 words.append(word)
     num_words = len(words)
     result["num_words"] = num_words
 
+
     num_correctly_spelled = 0
     for word in words:
         try:
-            if enchantDict.check(word):
+            if enchantDict.check(word) or NER_re.match(word) or word in CONTRACTIONS or word in SPECIAL_WORDS:
                 num_correctly_spelled += 1
+            # else:
+            #     print word.encode('utf-8')
         except enchant.errors.Error:
             print "can't spell check", word
     result["num_correctly_spelled"] = num_correctly_spelled
@@ -85,12 +100,17 @@ for row in input:
         + (11.8 * (num_syl / num_words)) - 15.59
     result["fk_grade_level"] = fk_grade_level
 
+    if words[0] == 'dear':
+        result["starts_with_dear"] = 1
+    else:
+        result["starts_with_dear"] = 0
+
     output.writerow(result)
 
-    print text
-    print sents
-    print words_in_sentances
-    print words
+    # print text
+    # print sents
+    # print words_in_sentances
+    # print words
 
     i+= 1
     if maxRows and i >= maxRows:
