ade9a1226c5078b5e1422854f6ad422f30d8647d
Chris Mulligan <clm2186@columbia.edu>
2012-11-11 21:25:08 -0500

    Random forest model + predictions.
    Suggests slightly increased accuracy. RF gives MQWK = 0.7433381,
    while LM MQWK = 0.7365452 on the holdout sample.

--- basicModel.R
+++ basicModel.R
@@ -1,6 +1,7 @@
 source("ASAP-AES/Evaluation_Metrics/R/quadratic_weighted_kappa.R")
 require(plyr)
 require(MASS)
+require(ggplot2)
 
 training <- read.csv("train_tagged.csv")
 training <- training[!is.na(training$grade),]
@@ -15,14 +16,13 @@ training$spell_pct <- training$num_correctly_spelled / training$num_words
 
 training$grade
 training$grade_log <- log(training$grade)
-hist(training$grade_log)
-plot(training$spell_pct, training$grade)
+qplot(grade, data=training, facets=set~., binwidth=1)
+
 
 cor(training, use="complete.obs")
 
 training$holdout <- 0
-training$holdout[sample(nrow(training), as.integer(nrow(training)*.1))] <- 1
-
+training$holdout[sample(nrow(training), as.integer(nrow(training)*.2))] <- 1
 
 
 models <- dlply(training[training$holdout==0,], .(set), lm, formula = grade ~ num_chars + log(num_chars) + avg_length + avg_syls  + spell_pct + starts_with_dear + spell_mistakes + sentance_length + num_superlatives + (distinct_words / num_words) + (num_nouns/num_adjectives) + (num_nouns/num_verbs) + (num_nouns/num_adverbs) + has_semicolon + has_exclamation + has_questionmark + num_foreign)
@@ -38,7 +38,6 @@ lapply(models, summary)
 #replace with something more elegant :(
 for (i in 1:max(training$set)) {
     training$scorehat[training$set==i] <- predict(models[[i]], training[training$set==i,])
-    training$scorehat[training$set==i && is.na(training$scorehat)] <- mean(training$grade[training$set==i])
 }
 training$prediction <- round(training$scorehat)
 training$residual <- training$grade - training$prediction
@@ -61,21 +60,38 @@ summary(training)
 
 #randomForest
 require(randomForest)
-rfm <- randomForest(grade ~ num_chars + num_sents + num_words + num_syl + sentance_length + avg_syls + spell_mistakes + fk_grade_level + starts_with_dear + distinct_words + end_with_preposition + num_nouns + num_verbs + num_adjectives + num_adverbs + num_conjunctions + num_prepositions + num_superlatives + avg_length + spell_pct + (distinct_words / num_words) + (num_nouns/num_adjectives) + (num_nouns/num_verbs) + (num_nouns/num_adverbs),
-              strata=set, data=training[training$holdout==0,], importance=TRUE, ntree=1500)
-training$rfprediction <- round(predict(rfm, training))
+#rfm <- randomForest(grade ~ num_chars + num_sents + num_words + num_syl + sentance_length + avg_syls + spell_mistakes + fk_grade_level + starts_with_dear + distinct_words + end_with_preposition + num_nouns + num_verbs + num_adjectives + num_adverbs + num_conjunctions + num_prepositions + num_superlatives + avg_length + spell_pct + (distinct_words / num_words) + (num_nouns/num_adjectives) + (num_nouns/num_verbs) + (num_nouns/num_adverbs),
+#              strata=set, data=training[training$holdout==0,], importance=TRUE, ntree=2500, mtry=5)
+#training$rfscorehat <- predict(rfm, training)
+
+
+rfms = list()
+for (i in 1:max(training$set)) {
+    rfms[[i]] <- randomForest(grade ~ num_chars + num_sents + num_words + num_syl + sentance_length + avg_syls + spell_mistakes + fk_grade_level + distinct_words + end_with_preposition + num_nouns + num_verbs + num_adjectives + num_adverbs + num_conjunctions + num_prepositions + num_superlatives + avg_length + spell_pct + (distinct_words / num_words) + (num_nouns/num_adjectives) + (num_nouns/num_verbs) + (num_nouns/num_adverbs),
+        data=training[training$set == i & training$holdout==0,], importance=TRUE, ntree=2000, mtry=5)
+    training$rfscorehat[training$set==i] <- predict(rfms[[i]], training[training$set==i,])
+}
+training$rfprediction <- round(training$rfscorehat)
+rfms
+
 kappasrfm <- dlply(training, .(set), function(X) ScoreQuadraticWeightedKappa(X$grade, X$rfprediction))
 kappasrfm <- dlply(training, .(set), function(X) ScoreQuadraticWeightedKappa(X$grade[X$holdout==1], X$rfprediction[X$holdout==1]))
 kappasrfm
 MeanQuadraticWeightedKappa(kappasrfm)
+
+
+training$avgpred <- round((training$scorehat + training$rfscorehat)/2)
+kappasavg <- dlply(training, .(set), function(X) ScoreQuadraticWeightedKappa(X$grade, X$avgpred))
+kappasavg <- dlply(training, .(set), function(X) ScoreQuadraticWeightedKappa(X$grade[X$holdout==1], X$avgpred[X$holdout==1]))
+kappasavg
+MeanQuadraticWeightedKappa(kappasavg)
+
 
 training[training$holdout==1 && training$grade != training$rfprediction,]
 
 
 
 ##SCORE
-
-
 testing <- read.csv("test_tagged.csv")
 testing <- testing[order(testing$set, testing$essay_id), ]
 
@@ -88,14 +104,13 @@ testing$spell_pct <- testing$num_correctly_spelled / testing$num_words
 
 for (i in 1:max(testing$set)) {
     testing$scorehat[testing$set==i] <- predict(models[[i]], testing[testing$set==i,])
-    testing$scorehat[testing$set==i && is.na(testing$scorehat)] <- mean(testing$grade[testing$set==i])
+    testing$rfscorehat[testing$set==i] <- predict(rfms[[i]], testing[testing$set==i,])
 }
-testing$scorehat[is.na(testing$scorehat)] = 1
+#testing$scorehat[is.na(testing$scorehat)] = 1
 testing$prediction <- round(testing$scorehat)
-hist(testing$prediction)
+testing$rfprediction <- round(testing$rfscorehat)
 
 testing$weight = 1
-write.csv(testing[, c("id", "set", "weight", "prediction")], "testing_predicted.csv", row.names=FALSE, na="")
+write.csv(testing[, c("id", "set", "weight", "prediction")], "testing_predicted_lm.csv", row.names=FALSE, na="")
+write.csv(testing[, c("id", "set", "weight", "rfprediction")], "testing_predicted_rf.csv", row.names=FALSE, na="")
 
-
-testing[testing$id==11832, ]
