c576b9379633b510dfc1e95c6904c6f33133c4e6
Chris Mulligan <clm2186@columbia.edu>
2012-12-11 00:01:00 -0500

    KAGGLE COMPETITION DONE!!!

--- validateSubmission.py
+++ validateSubmission.py
@@ -10,6 +10,15 @@ filename = sys.argv[1]
 csvfile = csv.reader(open(filename))
 ok = True
 
+extremes = [
+    None,
+    (2, 12),
+    (0, 4),
+    (0, 3),
+    (0, 3),
+    (0, 4),
+]
+
 headers = csvfile.next()
 if len(headers) != 4:
     ok = False
@@ -18,11 +27,21 @@ print "Assuming %s is id, %s is set, %s is weight, %s is predictedgrade" % (head
 
 i = 2
 for row in csvfile:
-    if row[3] == "" or not (int(row[3]) in range(12)):
+    pred = int(row[3])
+    if row[3] == "" or not (pred in range(12)):
         ok = False
         print "oh shit, issue with row %s:   %s" % (i, ','.join(row))
+    setN = int(row[1])
+    if pred < extremes[setN][0]:
+        ok = False
+        print "row %s has a score too low!   %s" % (i, ','.join(row)) 
+    elif pred > extremes[setN][1]:
+        ok = False
+        print "row %s has a score too high!   %s" % (i, ','.join(row)) 
     i += 1
+
 print "Found %s rows. %s\n" % (i-1, "same" if i-1 == origLength else "DIFFERENT!")
+
 if i-1 != origLength:
     ok = False
 if ok:
